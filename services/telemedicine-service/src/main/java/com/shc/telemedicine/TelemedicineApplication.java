04b7f657071efec3950ddf5e4d5804dc71b5decf
Mahen Perera <kmaperera864@gmail.com>
2026-03-24 22:21:04 +0530

    Initial commit

---
+++ services/telemedicine-service/src/main/java/com/shc/telemedicine/TelemedicineApplication.java
@@ -0,0 +1,13 @@
+package com.shc.telemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedicineApplication.class, args);
+	}
+
+}
